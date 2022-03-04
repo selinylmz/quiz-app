6d544d95395d7465f89c6f839f17db219ac3d764
selinylmz <54477284+selinylmz@users.noreply.github.com>
2022-03-04 22:42:26 +0300

    guess answer

--- app.js
+++ app.js
@@ -9,7 +9,35 @@ function Question(text, choices, answer) {
 Question.prototype.checkAnswer = function (answer) {
   return this.answer === answer;
 };
+//Quiz Constructor
 
+function Quiz(questions) {
+  this.questions = questions;
+  this.score = 0;
+  this.questionIndex = 0;
+}
+
+//Quiz Prototype
+
+Quiz.prototype.getQuestion = function () {
+  return this.questions[this.questionIndex];
+};
+//Quiz Finish
+
+Quiz.prototype.isFinish = function () {
+  return this.questions.length === this.questionIndex;
+};
+//Quiz answer guess
+
+Quiz.prototype.guess = function (answer) {
+  debugger;
+  var question = this.getQuestion();
+  if (question.checkAnswer(answer)) {
+    debugger;
+    this.score++;
+  }
+  this.questionIndex++;
+};
 
 let q1 = new Question(
   "What is the best programming ?",
@@ -26,8 +54,44 @@ let q3 = new Question(
   ["C#", "javascript", "pyhton", "asp.net"],
   "javascript"
 );
+var questions = [q1, q2, q3];
+//Start Quiz
+var quiz = new Quiz(questions);
 
+loadQuestion();
 
+function loadQuestion() {
+  if (quiz.isFinish()) {
+    showScore();
+  } else {
+    var question = quiz.getQuestion();
+    var choices = question.choices;
 
-console.log(q1.checkAnswer("C#"));
+    document.querySelector("#question").textContent = question.text;
+    for (var i = 0; i < choices.length; i++) {
+      var element = document.querySelector(`#choice${i}`);
+      element.innerHTML = choices[i];
+      guess(`btn${i}`, choices[i]);
+    }
+    showProgress();
+  }
+}
+function guess(id, guess) {
+  var btn = document.getElementById(id);
+  btn.onclick = function () {
+    quiz.guess(guess);
+    loadQuestion();
+  };
+}
+function showScore() {
+  var html = `<h2>Score</h2><h4>${quiz.score}</h4>`;
+  document.querySelector(".card-body").innerHTML = html;
+}
+function showProgress() {
+  var totalQuestion = quiz.questions.length;
+  var questionNumber = quiz.questionIndex + 1;
 
+  document.querySelector(
+    "#progress"
+  ).innerHTML = `Question ${questionNumber} of ${totalQuestion}`;
+}
